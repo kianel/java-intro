5b7b3544649cce3540a75a666d198b43965baab5
kianel <l.kianel@gmail.com>
2018-03-13 19:33:18 +0200

    initial commit

---
+++ src/lesson01/HelloWorld.java
@@ -0,0 +1,21 @@
+package lesson01;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        boolean bo = true;
+        int i = 2;
+        long l = 5000000008L;
+        float f = 9.5f;
+        double d = 9.5;
+        char c = 'f';
+        System.out.println(bo);
+        System.out.println(c);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+
+
+    }
+}
